4a9a4da10098e6179c43c260ce38c12f69922ddd
wasioto <wasioto141@gmail.com>
2024-11-04 23:09:31 -0500

    Ceaser Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,44 @@
 # add your code here
+#Right shift of five (A will be F)
+#Please enter a sentence: python is fun!
+#The encrypted sentence is: udymts nx kzs!
+shift = {
+    "a" : "f",
+    "b " : "g",
+    "c" : "h",
+    "d" : "i",
+    "e" : "j",
+    "f" : "k",
+    "g" : "l",
+    "h" : "m",
+    "i" : "n",
+    "j" : "o",
+    "k" : "p", 
+    "l" : "q", 
+    "m" : "r", 
+    "n" : "s", 
+    "o" : "t", 
+    "p" : "u", 
+    "q" : "v", 
+    "r" : "w",
+    "s" : "x", 
+    "t" : "y",
+    "u" : "z",
+    "v" : "a",
+    "w" : "b",
+    "x" : "c",
+    "y" : "d",
+    "z" : "e", 
+    }
+
+text = input("Please enter a sentence")
+text = text.lower()
+cipher_text = ""
+for char in text:
+    if char in shift:
+        char = shift[char]
+    cipher_text += char
+print(cipher_text)
+
+
+
